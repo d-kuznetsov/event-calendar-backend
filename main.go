ab04768e932df7f2780b90d159cf39cc5af08b42
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-10 01:12:40 +0200

    start server 0.1

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"fmt"
+	_ "fmt"
+	_ "log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/d-kuznetsov/calendar-backend/handler"
+	"github.com/d-kuznetsov/calendar-backend/repository"
+	"github.com/d-kuznetsov/calendar-backend/service"
 )
 
-func handler(writer http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(writer, "Some message")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	client := repository.CreateMongoClient("mongodb://localhost:27017")
+	service := service.Service{
+		Repository: repository.CreateMongoRepo(client, "calendar"),
+	}
+	router.HandleFunc("/register", handler.CreateHttpHandler(handler.RegisterHandler, &service)).Methods("POST")
+	http.ListenAndServe(":8080", router)
 }
