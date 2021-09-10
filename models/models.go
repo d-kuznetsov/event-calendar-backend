667316f1d1185db3089fe60e076c5487f11fb0bb
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-10 17:07:27 +0200

    add Event model

--- models/models.go
+++ models/models.go
@@ -6,3 +6,12 @@ type User struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type Event struct {
+	Id        string `json:"id"`
+	Date      string `json:"date"`
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
+	Content   string `json:"content"`
+	UserId    string `json:"userId"`
+}
