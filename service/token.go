09afd1219556cdc24d0a77de1bd87d727288b4b6
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-11 19:53:55 +0200

    update generateToken; create parseToken

--- service/token.go
+++ service/token.go
@@ -1,32 +1,41 @@
 package service
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-var signingKey = []byte("test_key")
+var signingKey = "test_key"
 
 const tokenTTL = 12 * time.Hour
 
-type tokenClaims struct {
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	jwt.StandardClaims
+func generateToken(email string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+		Subject:   email,
+	})
+	return token.SignedString([]byte(signingKey))
 }
 
-func generateToken(id, name, email string) (string, error) {
-	claims := tokenClaims{
-		id,
-		name,
-		email,
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
-			IssuedAt:  time.Now().Unix(),
-		},
+func parseToken(tokenStr string) (string, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (i interface{}, err error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return "", err
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(signingKey)
+	//token.Claims.Valid()
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("error get user claims from token")
+	}
+
+	return claims["sub"].(string), nil
 }
