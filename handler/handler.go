93bae72003700960fb1a55e5a1dd855cad14a748
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-11 22:49:41 +0200

    add CreateEventHandler

--- handler/handler.go
+++ handler/handler.go
@@ -55,10 +55,41 @@ type Credentials struct {
 	Password string
 }
 
+func CreateEventHandler(wtr http.ResponseWriter, req *http.Request, svc service.IService) {
+	token := extractToken(req)
+	userId, err := svc.ParseToken(token)
+	if err != nil {
+		throw401Error(wtr)
+		return
+	}
+	len := req.ContentLength
+	body := make([]byte, len)
+	req.Body.Read(body)
+	eventOpts := service.EventOpts{}
+	json.Unmarshal(body, &eventOpts)
+	eventOpts.UserId = userId
+	eventId, err := svc.CreateEvent(eventOpts)
+	if err != nil {
+		throw500Error(wtr)
+		return
+	}
+	wtr.Write([]byte(eventId))
+}
+
 func throw400Error(wtr http.ResponseWriter, errMsg string) {
 	http.Error(wtr, errMsg, http.StatusBadRequest)
 }
 
+func throw401Error(wtr http.ResponseWriter) {
+	http.Error(wtr, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+}
+
 func throw500Error(wtr http.ResponseWriter) {
 	http.Error(wtr, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func extractToken(req *http.Request) string {
+	prefix := "Bearer "
+	authHeader := req.Header.Get("Authorization")
+	return authHeader[len(prefix):]
 }
