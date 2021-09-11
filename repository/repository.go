6162595da73ae5e7811293e66f95cb93ed246fda
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-09-11 22:44:58 +0200

    add CreateEvent method to interface

--- repository/repository.go
+++ repository/repository.go
@@ -9,6 +9,15 @@ import (
 type IRepository interface {
 	CreateUser(name, email, hashedPassword string) (string, error)
 	GetUserByEmail(email string) (models.User, error)
+	CreateEvent(opts EventOpts) (string, error)
+}
+
+type EventOpts struct {
+	Date      string
+	StartTime string
+	EndTime   string
+	Content   string
+	UserId    string
 }
 
 var ErrNoUsersFound = errors.New("repository error: no users found")
